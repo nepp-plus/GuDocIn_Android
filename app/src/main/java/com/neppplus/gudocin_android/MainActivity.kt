bfa2267353508a0f5c2ff7093e79b659b2929346
nepp-plus <nepp.plus@gmail.com>
2021-11-23 12:09:17 +0900

    메인화면 - 데이터바인딩 세팅

--- app/src/main/java/com/neppplus/gudocin_android/MainActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/MainActivity.kt
@@ -2,10 +2,15 @@ package com.neppplus.gudocin_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
