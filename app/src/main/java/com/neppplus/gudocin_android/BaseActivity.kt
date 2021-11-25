cc30985428cbad7696e140638b7bd00dc419d700
nepp-plus <nepp.plus@gmail.com>
2021-11-25 17:35:13 +0900

    API 기본세팅

--- app/src/main/java/com/neppplus/gudocin_android/BaseActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/BaseActivity.kt
@@ -3,13 +3,21 @@ package com.neppplus.gudocin_android
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.neppplus.gudocin_android.api.ServerAPI
+import com.neppplus.gudocin_android.api.ServerAPIInterface
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext: Context
+
+    lateinit var apiService: ServerAPIInterface
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIInterface::class.java)
 
     }
 
