e754fd6e3445bc93207cbd7aed14ebf95501b8be
이아현 <suk03029@gmail.com>
2021-11-26 15:20:22 +0900

    색상등록

--- app/src/main/java/com/neppplus/gudocin_android/fragments/ReviewListFragment.kt
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/ReviewListFragment.kt
@@ -1,11 +1,34 @@
 package com.neppplus.gudocin_android.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.R
+import com.neppplus.gudocin_android.databinding.FragmentReviewListBinding
+
 class ReviewListFragment : BaseFragment() {
+
+    lateinit var binding: FragmentReviewListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_review_list,container,false)
+        return binding.root
+
+    }
+
     override fun setupEvents() {
 
     }
 
     override fun setValues() {
+
+
 
     }
 }
