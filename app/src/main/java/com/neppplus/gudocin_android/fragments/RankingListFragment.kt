276b2d3ad4d98279ec5e352877821d58a8a29c2b
김민성 <kmsz5813@naver.com>
2021-11-26 15:03:15 +0900

    #3 프래그먼트 연결 및 리싸이클러뷰 xml 추가

---
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/RankingListFragment.kt
@@ -0,0 +1,38 @@
+package com.neppplus.gudocin_android.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.R
+import com.neppplus.gudocin_android.databinding.FragmentRakingListBinding
+
+class RankingListFragment : BaseFragment() {
+
+    lateinit var binding : FragmentRakingListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_raking_list, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
