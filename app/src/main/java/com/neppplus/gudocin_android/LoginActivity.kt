6b7c8ceb06f26336f130cee2ae2e4fc8aeccc743
김준기 <gugi329@daum.net>
2021-11-26 17:58:39 +0900

    LoginActivity xml + kt 진행중

---
+++ app/src/main/java/com/neppplus/gudocin_android/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.neppplus.gudocin_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+    }
+}
