a734a9726466a36a669541bdfdf1036818e6f621
이아현 <suk03029@gmail.com>
2021-11-26 14:48:24 +0900

    프레그먼트 생성

---
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/ReviewListFragment.kt
@@ -0,0 +1,11 @@
+package com.neppplus.gudocin_android.fragments
+
+class ReviewListFragment : BaseFragment() {
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
