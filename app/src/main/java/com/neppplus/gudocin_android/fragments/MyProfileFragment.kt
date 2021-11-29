8ffc27ad4ef5d7027c74751933e844c37522b073
ujin-Bang <qkddnwls14@gmail.com>
2021-11-29 10:17:22 +0900

    팀프로젝트 개인담당업무 - 개인정보 -> 개인정보 수정UI

--- app/src/main/java/com/neppplus/gudocin_android/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/MyProfileFragment.kt
@@ -1,7 +1,29 @@
 package com.neppplus.gudocin_android.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.gudocin_android.R
+import com.neppplus.gudocin_android.databinding.FragmentMyProfileBinding
 
-class MyProfileFragment : Fragment(R.layout.fragment_my_profile) {
+class MyProfileFragment : BaseFragment() {
+
+    lateinit var binding : FragmentMyProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+         onCreateView(inflater, container, savedInstanceState)
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
 }
