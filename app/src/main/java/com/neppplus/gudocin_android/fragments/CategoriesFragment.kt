e0ade308d944846221b4d1a843254c8c0aee092e
이아현 <suk03029@gmail.com>
2021-11-29 10:23:41 +0900

    카테고리 프레그먼트 kt파일

---
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/CategoriesFragment.kt
@@ -0,0 +1,57 @@
+package com.neppplus.gudocin_android.fragments
+
+import android.media.Image
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.neppplus.gudocin_android.R
+import com.neppplus.gudocin_android.adapters.BannerViewPagerAdapter
+import com.neppplus.gudocin_android.adapters.ReviewRecyclerViewAdapterForMain
+import com.neppplus.gudocin_android.databinding.BannerItemForMainBinding
+import com.neppplus.gudocin_android.databinding.FragmentReviewListBinding
+import com.neppplus.gudocin_android.datas.BasicResponse
+import com.neppplus.gudocin_android.datas.ReviewData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CategoriesFragment : BaseFragment() {
+
+    lateinit var binding: BannerItemForMainBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_categories,container,false)
+        return binding.root
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
+
