84565c6eec6ef1e2321c7bd1491ff322ebc09a96
ujin-Bang <qkddnwls14@gmail.com>
2021-11-30 12:31:41 +0900

    팀프로젝트 개인담당업무 - 개인정보 -> 고객센터 UI작업 + Activity연결

--- app/src/main/java/com/neppplus/gudocin_android/UserEditActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/UserEditActivity.kt
@@ -1,22 +1,20 @@
 package com.neppplus.gudocin_android
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.neppplus.gudocin_android.databinding.ActivitySaveMonyMyBinding
+import com.neppplus.gudocin_android.databinding.ActivityUserEditBinding
 
-class SaveMonyMyActivity : BaseActivity() {
+class UserEditActivity : BaseActivity() {
 
-    lateinit var binding : ActivitySaveMonyMyBinding
-
+    lateinit var binding : ActivityUserEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      binding= DataBindingUtil.setContentView(this,R.layout.activity_save_mony_my)
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_user_edit)
 
         setValues()
         setupEvents()
     }
-
-
 
 
 
--- app/src/main/java/com/neppplus/gudocin_android/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/MyProfileFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.MyProductPurchaseListActivity
 import com.neppplus.gudocin_android.R
 import com.neppplus.gudocin_android.SaveMonyMyActivity
+import com.neppplus.gudocin_android.UserEditActivity
 import com.neppplus.gudocin_android.databinding.FragmentMyProfileBinding
 
 class MyProfileFragment : BaseFragment() {
@@ -42,6 +44,29 @@ class MyProfileFragment : BaseFragment() {
             startActivity(myIntent)
 
         }
+
+        binding.txtEditUserInfo.setOnClickListener {
+
+            val myIntent = Intent(mContext, UserEditActivity::class.java)
+
+            startActivity(myIntent)
+        }
+
+        binding.txtUserPointDetails.setOnClickListener {
+
+            val myIntent =  Intent( mContext, SaveMonyMyActivity::class.java )
+
+            startActivity(myIntent)
+
+        }
+        binding.txtMyProductPurnchaseList.setOnClickListener {
+
+            val myIntent = Intent(mContext, MyProductPurchaseListActivity::class.java)
+
+            startActivity(myIntent)
+        }
+
+        binding.
 
     }
 
