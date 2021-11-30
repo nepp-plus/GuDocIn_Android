f8141101598d46ba667d5995e4e19c98b1d8136b
김준기 <gugi329@daum.net>
2021-11-30 10:07:04 +0900

    #19 진입화면 자동 슬라이드 구현

--- app/src/main/java/com/neppplus/gudocin_android/ApproachActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/ApproachActivity.kt
@@ -2,16 +2,30 @@ package com.neppplus.gudocin_android
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager.widget.ViewPager
 import com.neppplus.gudocin_android.adapters.ApproachViewPagerAdapter
 import com.neppplus.gudocin_android.databinding.ActivityApproachBinding
+import com.neppplus.gudocin_android.datas.BasicResponse
+import com.neppplus.gudocin_android.datas.GlobalData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ApproachActivity : BaseActivity() {
 
     lateinit var binding: ActivityApproachBinding
 
     internal lateinit var viewPager: ViewPager
+
+    var currentPosition = 0
+
+    val handler = Handler(Looper.getMainLooper()) {
+        setPage()
+        true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +38,26 @@ class ApproachActivity : BaseActivity() {
         val adapter = ApproachViewPagerAdapter(this)
         viewPager.adapter = adapter
 
+        val thread = Thread(PagerRunnable())
+        thread.start()
+
+    }
+
+    fun setPage() {
+
+        if (currentPosition == 3) currentPosition = 0
+        viewPager.setCurrentItem(currentPosition, true)
+        currentPosition += 1
+
+    }
+
+    inner class PagerRunnable : Runnable {
+        override fun run() {
+            while (true) {
+                Thread.sleep(3000)
+                handler.sendEmptyMessage(0)
+            }
+        }
     }
 
     override fun setupEvents() {
@@ -33,18 +67,35 @@ class ApproachActivity : BaseActivity() {
             val myIntent = Intent(mContext, LoginActivity::class.java)
             startActivity(myIntent)
 
-            binding.btnSignUp.setOnClickListener {
+        }
 
-                val myIntent = Intent(mContext, SignUpActivity::class.java)
-                startActivity(myIntent)
+        binding.btnSignUp.setOnClickListener {
 
-            }
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
 
         }
 
     }
 
     override fun setValues() {
+
+        apiService.getRequestMyInfo().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful) {
+
+                    GlobalData.loginUser = response.body()!!.data.user
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
 
     }
 
