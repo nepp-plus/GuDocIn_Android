c406f87e10488a109d53be6e078599853551be1a
nepp-plus <nepp.plus@gmail.com>
2021-11-30 11:16:25 +0900

    홈화면 구조 재편 작업 시작

---
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.neppplus.gudocin_android.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
