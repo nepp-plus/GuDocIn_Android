9b7d9a1b1ec7803def818681a8d54533c39efc5a
이아현 <suk03029@gmail.com>
2021-12-01 17:43:34 +0900

    #22 액션바 xml완성

--- app/src/main/java/com/neppplus/gudocin_android/BaseActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/BaseActivity.kt
@@ -2,20 +2,25 @@ package com.neppplus.gudocin_android
 
 import android.content.Context
 import android.os.Bundle
-import android.view.Menu
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.PagerAdapter
+import androidx.appcompat.widget.Toolbar
 import com.neppplus.gudocin_android.api.ServerAPI
 import com.neppplus.gudocin_android.api.ServerAPIInterface
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity() : AppCompatActivity() {
 
     lateinit var mContext: Context
 
     lateinit var apiService: ServerAPIInterface
 
-    var mActionBar= R.layout.custom_action_bar
+    lateinit var btnBack : ImageView
+    lateinit var btnBell : ImageView
+    lateinit var btnBasket : ImageView
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +29,55 @@ abstract class BaseActivity : AppCompatActivity() {
         val retrofit = ServerAPI.getRetrofit(mContext)
         apiService = retrofit.create(ServerAPIInterface::class.java)
 
-        mActionBar.cust
+        supportActionBar?.let {
+
+//            supportActionBar 이 변수가 null 아닐때만 해달라는 코드.
+            setCustomActionBar()
+
+
+        }
 
 
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        actionBar.inflate(R.layout.custom_action_bar)
-//    }
+    fun setCustomActionBar() {
+
+//        기본 액션바 가져오기 -> 액션바는 무조건 있다고 전제.
+        val defActionBar = supportActionBar!!
+
+//        이 액션바를 커스텀 모드로 변경
+        defActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+//        실제 커스텀뷰를 어떤 xml인지 설정
+        defActionBar.setCustomView(R.layout.custom_action_bar)
+
+//        좌우 여백 제거 : ToolBar 소환 -> 여백값 세팅
+        val toolBar =  defActionBar.customView.parent as Toolbar
+        toolBar.setContentInsetsAbsolute(0, 0)
+
+
+//       (액션바의 커스텀뷰에) 추가된 UI요소들을 멤버변수에 연결
+        btnBack = defActionBar.customView.findViewById(R.id.btnBack)
+        btnBell = defActionBar.customView.findViewById(R.id.btnBell)
+        btnBasket = defActionBar.customView.findViewById(R.id.btnBasket)
+
+//        모든 화면 공통 이벤트처리
+        btnBack.setOnClickListener {
+
+//            뒤로가기 : 무조건 지금 화면 종료
+            finish()
+        }
+
+//        프로필 설정 화면 이동
+
+
+
+
+    }
+
+
+
+
 
     abstract fun setupEvents()
     abstract fun setValues()
