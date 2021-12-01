e97f9a74264dc0ec4544ee5ad3c51c1f6e287ac0
김민성 <kmsz5813@naver.com>
2021-12-01 11:06:55 +0900

    #10 activtity로 변경

---
+++ app/src/main/java/com/neppplus/gudocin_android/ReviewDetailActivity.kt
@@ -0,0 +1,25 @@
+package com.neppplus.gudocin_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.databinding.ActivityReviewDetailBinding
+
+class ReviewDetailActivity : BaseActivity() {
+
+    lateinit var binding: ActivityReviewDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_review_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
