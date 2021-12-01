750b52f8a3366a49e43efe48df72a58f4e7dd9d8
ujin-Bang <qkddnwls14@gmail.com>
2021-12-01 18:12:47 +0900

    프래그먼트 / 뷰페이져 연결  - 뷰페이져 / 텝레이아웃 연결 작업 필요

--- app/src/main/java/com/neppplus/gudocin_android/MyPurchaseListActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/MyPurchaseListActivity.kt
@@ -2,15 +2,19 @@ package com.neppplus.gudocin_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.adapters.PurchaseViewPagerAdapter
 import com.neppplus.gudocin_android.databinding.ActivityMyPurchaseListBinding
 
 class MyPurchaseListActivity : BaseActivity() {
 
     lateinit var binding : ActivityMyPurchaseListBinding
 
+    lateinit var mAdapter: PurchaseViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_purchase_list)
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_my_purchase_list)
 
         setupEvents()
         setValues()
@@ -21,6 +25,9 @@ class MyPurchaseListActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mAdapter = PurchaseViewPagerAdapter(supportFragmentManager)
+        binding.purchaseViewPager.adapter = mAdapter
 
     }
 }
