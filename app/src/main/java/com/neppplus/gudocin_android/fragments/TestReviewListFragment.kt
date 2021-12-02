3e654381b2c7d59407a520c664e6735fc8e3a2ec
koolunkle <gugi329@daum.net>
2021-12-02 23:26:32 +0900

    테스트 리뷰 목록 프래그먼트 기초 작업

---
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/TestReviewListFragment.kt
@@ -0,0 +1,39 @@
+package com.neppplus.gudocin_android.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.R
+import com.neppplus.gudocin_android.databinding.FragmentTestReviewListBinding
+
+class TestReviewListFragment : BaseFragment() {
+
+    lateinit var binding: FragmentTestReviewListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_test_review_list, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
