67f3cc04109a8d3348284cc3a47ec215b7dcde99
이아현 <suk03029@gmail.com>
2021-12-02 12:22:12 +0900

    #22 상세화면 에 들어갈 프레그먼트 기본 세팅

---
+++ app/src/main/java/com/neppplus/gudocin_android/adapters/ProductContentViewPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.neppplus.gudocin_android.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.gudocin_android.fragments.ProductContentFragment
+import com.neppplus.gudocin_android.fragments.StoreContentFragment
+
+class ProductContentViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm){
+    override fun getCount( )= 2
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position) {
+
+            0 -> ProductContentFragment()
+                else -> StoreContentFragment()
+        }
+
+    }
+}
--- app/src/main/java/com/neppplus/gudocin_android/fragments/StoreContentFragment.kt
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/StoreContentFragment.kt
@@ -7,36 +7,36 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.neppplus.gudocin_android.R
 import com.neppplus.gudocin_android.adapters.ProductContentViewPagerAdapter
-import com.neppplus.gudocin_android.databinding.FragmentProductDetailBinding
+import com.neppplus.gudocin_android.databinding.FragmentStoreContentBinding
 
-class ProductDetailFragment :BaseFragment() {
+class StoreContentFragment : BaseFragment() {
 
-    lateinit var binding:FragmentProductDetailBinding
-    lateinit var mProductContentViewPagerAdapter : ProductContentViewPagerAdapter
+    lateinit var binding : FragmentStoreContentBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate( inflater, R.layout.fragment_product_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_store_content,container,false)
         return binding.root
-
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         setupEvents()
         setValues()
-    }
 
+
+    }
     override fun setupEvents() {
 
     }
 
     override fun setValues() {
-        mProductContentViewPagerAdapter = ProductContentViewPagerAdapter(childFragmentManager)
-        binding.ProductContentViewPager.adapter= mProductContentViewPagerAdapter
-        binding.ProductContentTabLayout.setupWithViewPager( binding.ProductContentViewPager )
+
+
+
     }
 }
