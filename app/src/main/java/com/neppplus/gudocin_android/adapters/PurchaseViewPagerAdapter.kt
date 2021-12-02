27be897d5595fcd65433695ee9c414849d674281
koolunkle <gugi329@daum.net>
2021-12-02 23:27:22 +0900

    2개의 프래그먼트를 뿌려줄 어댑터 작업

--- app/src/main/java/com/neppplus/gudocin_android/adapters/PurchaseViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/gudocin_android/adapters/PurchaseViewPagerAdapter.kt
@@ -3,19 +3,19 @@ package com.neppplus.gudocin_android.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.neppplus.gudocin_android.fragments.MyMyReviewListFragment
-import com.neppplus.gudocin_android.fragments.PurchaseListFragment
+import com.neppplus.gudocin_android.fragments.PurchastListFragment
+import com.neppplus.gudocin_android.fragments.TestReviewListFragment
 
-class PurchaseViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm){
-    override fun getCount( )= 2
+class PurchaseViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount() = 2
 
     override fun getItem(position: Int): Fragment {
 
-        return when(position) {
-
-            0 -> PurchaseListFragment()
-                else -> MyMyReviewListFragment()
+        return when (position) {
+            0 -> PurchastListFragment()
+            else -> TestReviewListFragment()
         }
 
     }
+
 }
