922d9e741419063a40e4195067fbf7c52a5f393f
김준기 <gugi329@daum.net>
2021-12-02 14:42:31 +0900

    아이디 찾기 진행중

--- app/src/main/java/com/neppplus/gudocin_android/FindAccountActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/FindAccountActivity.kt
@@ -1,8 +1,15 @@
 package com.neppplus.gudocin_android
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.gudocin_android.databinding.ActivityFindAccountBinding
+import com.neppplus.gudocin_android.datas.BasicResponse
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FindAccountActivity : BaseActivity() {
 
@@ -18,6 +25,48 @@ class FindAccountActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnSubmit1.setOnClickListener {
+
+            val inputNickname = binding.edtNickname1.text.toString()
+            val inputPhone = binding.edtPhone.text.toString()
+
+            apiService.getRequestEmail(inputNickname, inputPhone).enqueue(object :
+                Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                    if (response.isSuccessful) {
+
+                        val basicResponse = response.body()!!
+
+                        val userEmail = basicResponse.data.user.email
+
+                        Toast.makeText(mContext, "찾으시는 아이디는 ${userEmail}입니다.", Toast.LENGTH_SHORT)
+                            .show()
+
+                    } else {
+
+                        val errorJson = JSONObject(response.errorBody()!!.string())
+                        Log.d("에러경우", errorJson.toString())
+
+                        val message = errorJson.getString("message")
+
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
+
+        }
 
     }
 
