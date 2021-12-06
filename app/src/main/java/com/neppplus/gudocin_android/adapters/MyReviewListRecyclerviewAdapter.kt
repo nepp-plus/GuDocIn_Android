a82aabd36debfc6ebe0a340a30fb0ef970075e3e
yeonjin seong <syj3405@naver.com>
2021-12-06 16:52:12 +0900

    나의 리뷰목록 어댑터 만드는중 리뷰목록 데이터 어떻게 받아오나요?

---
+++ app/src/main/java/com/neppplus/gudocin_android/adapters/MyReviewListRecyclerviewAdapter.kt
@@ -0,0 +1,18 @@
+package com.neppplus.gudocin_android.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyReviewListRecyclerviewAdapter : RecyclerView.Adapter<>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
+
+    }
+
+    override fun onBindViewHolder(holder: ???, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+}
