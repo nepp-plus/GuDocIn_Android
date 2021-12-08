d120bfb6b097e822ded69d7ee34ca30f595466ee
이아현 <suk03029@gmail.com>
2021-12-08 18:21:39 +0900

    #27 도전 중

--- app/src/main/java/com/neppplus/gudocin_android/fragments/RankingFragment.kt
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/RankingFragment.kt
@@ -1,5 +1,6 @@
 package com.neppplus.gudocin_android.fragments
 
+import android.R.attr
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,12 @@ import com.neppplus.gudocin_android.datas.ReviewData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
+import kotlin.collections.ArrayList
+import android.R.attr.data
+
+
+
 
 class RankingFragment : BaseFragment() {
 
@@ -44,6 +51,7 @@ class RankingFragment : BaseFragment() {
 
     override fun setupEvents() {
 
+
     }
 
     override fun setValues() {
@@ -53,6 +61,10 @@ class RankingFragment : BaseFragment() {
         mReviewAdapter = RankingRecyclerViewListAdapter(mContext, mRankingList)
         binding.rankingRecyclerView.adapter = mReviewAdapter
         binding.rankingRecyclerView.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+
+//        binding.btnSortData.setOnClickLisner{
+//            sortData()
+//        }
 
     }
 
@@ -68,6 +80,12 @@ class RankingFragment : BaseFragment() {
                     mRankingList.clear()
                     mRankingList.addAll( br.data.reviews )
 
+//                    //정렬 바꿔보기
+//                    Collections.sort(br,Comparator<Any?> { lhs, rhs ->
+//                        // -1 - less than, 1 - greater than, 0 - equal, all inversed for descending
+//                            if (lhs.getId() > rhs.getId()) -1 else if (lhs.customInt < rhs.customInt) 1 else 0
+//                        })
+
                     mReviewAdapter.notifyDataSetChanged()
                 }
 
@@ -81,6 +99,17 @@ class RankingFragment : BaseFragment() {
         })
 
     }
+
+//    fun sortData(condition: Boolean){
+//
+//        if (condition) {
+//            mRankingList.sort()
+//        } else {
+//            mRankingList.reverse()
+//        }
+//        mReviewAdapter.notifyDataSetChanged()
+//
+//    }
 
 
 }
