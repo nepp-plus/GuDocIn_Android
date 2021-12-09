32acf14a79187fa78b6ddfcace74e2f5d2964c47
김준기 <gugi329@daum.net>
2021-12-09 12:05:33 +0900

    #23 장바구니 리스트 삭제 기능 구현 과정에서 enqueue 오류

--- app/src/main/java/com/neppplus/gudocin_android/adapters/BasketRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/gudocin_android/adapters/BasketRecyclerAdapter.kt
@@ -1,18 +1,23 @@
 package com.neppplus.gudocin_android.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.facebook.CallbackManager
 import com.neppplus.gudocin_android.R
 import com.neppplus.gudocin_android.api.ServerAPI
 import com.neppplus.gudocin_android.api.ServerAPIInterface
 import com.neppplus.gudocin_android.datas.BasicResponse
 import com.neppplus.gudocin_android.datas.BasketData
+import com.neppplus.gudocin_android.datas.ProductData
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,6 +35,8 @@ class BasketRecyclerAdapter(val mContext: Context, val mList: List<BasketData>) 
 
         val retrofit = ServerAPI.getRetrofit(mContext)
 
+        lateinit var mProductData: ProductData
+
         val imgDeleteSubscribe = view.findViewById<ImageView>(R.id.imgDeleteSubscribe)
 
         fun bind(data: BasketData) {
@@ -44,6 +51,37 @@ class BasketRecyclerAdapter(val mContext: Context, val mList: List<BasketData>) 
             apiService = retrofit.create(ServerAPIInterface::class.java)
 
             imgDeleteSubscribe.setOnClickListener {
+
+                apiService.deleteRequestProduct(mProductData.id).enqueue(object : Callback<BasicResponse> {
+                    override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                        if (response.isSuccessful) {
+
+                            Toast.makeText(
+                                mContext,
+                                "장바구니 목록이 삭제되었습니다",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+
+                        } else {
+
+                            val errorJson = JSONObject(response.errorBody()!!.string())
+                            Log.d("에러경우", errorJson.toString())
+
+                            val message = errorJson.getString("message")
+
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                        }
+
+                    }
+
+                    override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                    }
+
+                })
 
             }
 
