d827fb5cd2870c04e4b047b34b31cd7eaae7c8aa
koolunkle <gugi329@daum.net>
2022-01-09 12:46:30 +0900

    메인 화면 백버튼 기능 추가

--- app/src/main/java/com/neppplus/gudocin_android/NavigationActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/NavigationActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Base64
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -27,11 +29,26 @@ class NavigationActivity : BaseActivity() {
 
     lateinit var binding: ActivityNavigationBinding
 
+    var backKeyPressedTime: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_navigation)
         setupEvents()
         setValues()
+    }
+
+    override fun onBackPressed() {
+
+        if (System.currentTimeMillis() - backKeyPressedTime >= 1500) {
+            backKeyPressedTime = System.currentTimeMillis()
+            Toast.makeText(this, "'뒤로' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.finishAffinity(this)
+            System.runFinalization()
+            System.exit(0)
+        }
+
     }
 
     override fun setupEvents() {
