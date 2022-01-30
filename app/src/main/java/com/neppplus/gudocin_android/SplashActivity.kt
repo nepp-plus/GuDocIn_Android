b1b3cf41fd8e37c93296152c701c4c721a355bfd
김준기 <gugi329@daum.net>
2022-01-30 15:57:24 +0900

    SplashActivity 추가

---
+++ app/src/main/java/com/neppplus/gudocin_android/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.neppplus.gudocin_android
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.databinding.DataBindingUtil
+import com.neppplus.gudocin_android.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+            val myIntent = Intent(mContext, InitialActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 2500)
+    }
+
+}
