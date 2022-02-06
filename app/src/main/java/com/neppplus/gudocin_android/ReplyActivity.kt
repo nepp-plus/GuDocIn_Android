27be46ab344c75beabe6bfa7ef98a63e453eb67c
김준기 <gugi329@daum.net>
2022-02-06 23:44:40 +0900

    댓글 작성 유저 프로필 사진 추가

--- app/src/main/java/com/neppplus/gudocin_android/ReplyActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/ReplyActivity.kt
@@ -3,9 +3,11 @@ package com.neppplus.gudocin_android
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.neppplus.gudocin_android.adapters.ReplyAdapter
 import com.neppplus.gudocin_android.databinding.ActivityReplyBinding
 import com.neppplus.gudocin_android.datas.BasicResponse
+import com.neppplus.gudocin_android.datas.GlobalData
 import com.neppplus.gudocin_android.datas.ReplyData
 import com.neppplus.gudocin_android.datas.ReviewData
 import retrofit2.Call
@@ -39,10 +41,10 @@ class ReplyActivity : BaseActivity() {
                         response: Response<BasicResponse>
                     ) {
                         if (response.isSuccessful) {
-                            Toast.makeText(mContext, "댓글 작성에 성공했습니다", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, "댓글이 작성되었습니다", Toast.LENGTH_SHORT).show()
                             getReplyListFromServer()
                         } else {
-                            Toast.makeText(mContext, "서버 통신오류: 관리자에게 문의해주세요", Toast.LENGTH_SHORT)
+                            Toast.makeText(mContext, "댓글 작성 실패: 관리자에게 문의해주세요", Toast.LENGTH_SHORT)
                                 .show()
                         }
                     }
@@ -55,10 +57,28 @@ class ReplyActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        getMyInfoFromServer()
+        Glide.with(mContext).load(GlobalData.loginUser!!.profileImageURL).into(binding.imgProfile)
+
         mReviewData = intent.getSerializableExtra("review") as ReviewData
         mReplyAdapter = ReplyAdapter(mContext, R.layout.reply_list_item, mReplyList)
         binding.reviewReplyListview.adapter = mReplyAdapter
         getReplyListFromServer()
+    }
+
+    fun getMyInfoFromServer() {
+        apiService.getRequestMyInfo().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+                    val br = response.body()!!
+                    Glide.with(mContext).load(br.data.user.profileImageURL).into(binding.imgProfile)
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
     }
 
     fun getReplyListFromServer() {
