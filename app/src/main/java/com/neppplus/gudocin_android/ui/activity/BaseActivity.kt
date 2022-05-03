4d450e6c5e8fafd6cade005a9851d42a7dce1304
jungikim <2015110768@dgu.ac.kr>
2022-05-03 17:03:03 +0900

    불필요 파일 및 코드 삭제

--- app/src/main/java/com/neppplus/gudocin_android/ui/activity/BaseActivity.kt
+++ app/src/main/java/com/neppplus/gudocin_android/ui/activity/BaseActivity.kt
@@ -14,67 +14,64 @@ import com.neppplus.gudocin_android.api.ServerAPI
 import com.neppplus.gudocin_android.api.ServerAPIInterface
 
 abstract class BaseActivity : AppCompatActivity() {
-    lateinit var mContext: Context
-    lateinit var apiService: ServerAPIInterface
+  lateinit var mContext: Context
+  lateinit var apiService: ServerAPIInterface
 
-    lateinit var btnBack: ImageView
-    lateinit var btnAdd: ImageView
-    lateinit var btnCart: ImageView
+  lateinit var btnBack: ImageView
+  lateinit var txtTitleInActionBar: TextView
+  lateinit var btnShopping: ImageView
+  lateinit var btnCart: ImageView
 
-    lateinit var btnShopping: ImageView
-    lateinit var txtTitleInActionBar: TextView
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    mContext = this
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        mContext = this
+    val retrofit = ServerAPI.getRetrofit(mContext)
+    apiService = retrofit.create(ServerAPIInterface::class.java)
 
-        val retrofit = ServerAPI.getRetrofit(mContext)
-        apiService = retrofit.create(ServerAPIInterface::class.java)
-
-        supportActionBar?.let {
-            setCustomActionBar()
-        }
+    supportActionBar?.let {
+      setCustomActionBar()
     }
+  }
 
-    fun setCustomActionBar() {
+  private fun setCustomActionBar() {
 //        기본 액션바 가져오기 -> 액션바는 무조건 있다고 전제
-        val defActionBar = supportActionBar!!
-        Log.d("액션바", "설정으로 들어옴")
+    val defActionBar = supportActionBar!!
+    Log.d("액션바", "설정으로 들어옴")
 
 //        이 액션바를 커스텀 모드로 변경
-        defActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+    defActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
 
 //        실제 커스텀뷰를 어떤 xml 인지 설정
-        defActionBar.setCustomView(R.layout.custom_action_bar)
+    defActionBar.setCustomView(R.layout.custom_action_bar)
 
 //        좌우 여백 제거 : ToolBar 소환 -> 여백값 세팅
-        val toolBar = defActionBar.customView.parent as Toolbar
-        toolBar.setContentInsetsAbsolute(0, 0)
+    val toolBar = defActionBar.customView.parent as Toolbar
+    toolBar.setContentInsetsAbsolute(0, 0)
 
 //       (액션바의 커스텀뷰에) 추가된 UI 요소들을 멤버변수에 연결
-        btnBack = defActionBar.customView.findViewById(R.id.btnBack)
-        btnCart = defActionBar.customView.findViewById(R.id.btnCart)
-        btnAdd = defActionBar.customView.findViewById(R.id.btnAdd)
-        btnShopping = defActionBar.customView.findViewById(R.id.btnShopping)
-        txtTitleInActionBar =
-            defActionBar.customView.findViewById(R.id.txtTitleInActionBar)
+    btnBack = defActionBar.customView.findViewById(R.id.btnBack)
+    txtTitleInActionBar =
+      defActionBar.customView.findViewById(R.id.txtTitleInActionBar)
+    btnShopping = defActionBar.customView.findViewById(R.id.btnShopping)
+    btnCart = defActionBar.customView.findViewById(R.id.btnCart)
 
-        btnBack.setOnClickListener {
-            finish()
-        }
-
-        btnCart.setOnClickListener {
-            val myIntent = Intent(mContext, CartActivity::class.java)
-            startActivity(myIntent)
-        }
-
-        btnShopping.setOnClickListener {
-            val myIntent = Intent(mContext, ShoppingActivity::class.java)
-            startActivity(myIntent)
-        }
+    btnBack.setOnClickListener {
+      finish()
     }
 
-    abstract fun setupEvents()
-    abstract fun setValues()
+    btnCart.setOnClickListener {
+      val myIntent = Intent(mContext, CartActivity::class.java)
+      startActivity(myIntent)
+    }
+
+    btnShopping.setOnClickListener {
+      val myIntent = Intent(mContext, ShoppingActivity::class.java)
+      startActivity(myIntent)
+    }
+  }
+
+  abstract fun setupEvents()
+  abstract fun setValues()
 
 }
