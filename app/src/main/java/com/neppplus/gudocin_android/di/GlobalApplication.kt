2830a22bec37119ac7b8036b52cfaa5d2abf5fcc
jungikim <2015110768@dgu.ac.kr>
2022-06-13 16:03:40 +0900

    [2022-06-13] and the rest

---
+++ app/src/main/java/com/neppplus/gudocin_android/di/GlobalApplication.kt
@@ -0,0 +1,12 @@
+package com.neppplus.gudocin_android.di
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.neppplus.gudocin_android.R
+
+class GlobalApplication : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    KakaoSdk.init(this, getString(R.string.kakao_app_key))
+  }
+}
